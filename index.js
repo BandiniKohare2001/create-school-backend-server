1d6295246eec70e6b9e3aa89cfa3f327a3a3a566
Bandini2001 <bandinikohare30@gmail.com>
2023-10-20 00:55:41 +0530

    add get method

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
+const PORT = 5000;
+const students = []
 
-app.get('/health', (req, res)=>{
-    res.json({status: 'all good'})
+app.get('/health', (req, res) => {
+    res.json({ status: 'all good' })
 })
 
-const PORT = 5000;
+app.get('/students', (req, res) => {
+    
+    res.json({
+        success: true,
+        data: students,
+        message: 'Successfully fetch all students'
+    });
+
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`server runnig on port ${PORT}.`);
 });
