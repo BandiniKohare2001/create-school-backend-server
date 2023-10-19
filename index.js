fab496f2c2b5b7ce350dc9ac56a8ba85fef04655
Bandini2001 <bandinikohare30@gmail.com>
2023-10-20 01:32:16 +0530

    add post and fetch read feature

--- index.js
+++ index.js
@@ -20,6 +20,92 @@ app.get('/students', (req, res) => {
 });
 
 
+app.post('/student',(req,res)=>{
+    const {name,age,city,mobile}=req.body
+      const id=Math.floor(Math.random()*100000)
+      if(!name){
+        return res.json({
+          status:'error',
+          message:"please add name of Student"
+        })
+      }
+      if(!age){
+        return res.json({
+          status:'error',
+          message:"please add age of Student"
+        })
+      }
+      if(!city){
+        return res.json({
+          status:'error',
+          message:"please add city of Student"
+        })
+      }
+      if(!mobile){
+        return res.json({
+          status:'error',
+          message:"please add mobile number of Student"
+        })
+        
+      }
+      const obj={
+        id:id,
+        name:name,
+        age:age,
+        city:city,
+        mobile:mobile
+        
+      }
+
+      students.push(obj)
+
+    res.json({
+        status:'ok',
+        list:obj,
+        message:'New Student Added Successfully'
+    })
+ })
+
+
+ app.get('/student',(req,res)=>{
+    
+    const {name} = req.query;
+    let result=null;
+    students.forEach((obj)=>{
+      if(obj.name===name){
+        result=obj;
+      }
+    })
+
+    if(result===null){
+        return res.json({
+            status: 'error',
+            message: "data not found"
+
+        })
+    }
+
+    res.json({
+      status:'ok',
+      data:result,
+      massage:'Data fetched Successfully'
+    })
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`server runnig on port ${PORT}.`);
